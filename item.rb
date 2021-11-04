f4e0dc7d79557db7e2c5dacfde8c7bcd162dfe21
LOctavio <luis_ram1997@hotmail.com>
2021-11-03 21:24:41 -0600

    Add Item class properties

--- item.rb
+++ item.rb
@@ -1,2 +1,12 @@
 class Item
+  def initialize(id, publish_date, achived)
+    @id = id
+    @label = nil
+    @publish_date = publish_date
+    @achived = achived
+  end
+
+  def label=(label)
+    @label = label
+  end
 end
